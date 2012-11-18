d7f99df6e9f2e5a93dd32c162cca4287fd1a5005
twer <twer@localhost.(none)>
2012-11-18 10:15:30 +0800

    jtong - upgrade version to 0.0.2, add include_js method to load js file automatically, only load file under 'js' folder expect sub folder

--- lib/jt-partial.rb
+++ lib/jt-partial.rb
@@ -1,14 +1,40 @@
 require "jt-partial/version"
 require "haml"
 
+#js
+
+def base_js(file)
+  is_base_file = file.include?( "application.js" ) || file.include?("pages.js")
+  puts file
+  puts is_base_file
+  is_base_file
+end
+
+def include_js
+  template_path = File.expand_path('../', __FILE__)
+  template = ""
+  Dir.glob "js/*.js" do |file|
+    if ! base_js file
+      template += haml_render "#{template_path}/_js_script_tag.haml",  {js_file_path: file}
+    end
+  end
+  template += haml_render "#{template_path}/_js_script_tag.haml",  {js_file_path: "js/pages.js"}
+  template
+end
+
+def haml_render(template_path, args = {})
+  Haml::Engine.new(File.read(template_path)).render Object.new, args
+end
+
+#partial
 def render(partial)
-  Haml::Engine.new(File.read("_#{partial}.haml")).render
+  partial_path = "_#{partial}.haml"
+  haml_render(partial_path)
 end
 
 def generate(template_name)
-  template = File.read("_#{template_name}.haml")
-  haml_engine = Haml::Engine.new(template)
-  output = haml_engine.render
+  template_path = "_#{template_name}.haml"
+  output = haml_render(template_path)
   html_page = File.new("#{template_name}.html", "w")
   html_page.write(output)
 end
--- lib/jt-partial/version.rb
+++ lib/jt-partial/version.rb
@@ -1,5 +1,5 @@
 module Jt
   module Partial
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
